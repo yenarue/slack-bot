e8bd552f2186e135dd967817f5ad6bade2eb54ab
yenarue <yenarue@gmail.com>
2018-05-07 02:01:54 +0900

    feat: Add find specific messages in the specific channel

--- controller/tutorial.js
+++ controller/tutorial.js
@@ -13,4 +13,30 @@ const getCurrentTime = (req, res) => {
     });
 };
 
-module.exports = {getCurrentTime};
+const findMessageInChannel = (req, res) => {
+    const msg = req.query.msg;
+    const channel = req.query.channel;
+    const incomingwebhook = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL);
+    const webClient = new WebClient(process.env.SLACK_TOKEN);
+
+    console.log('Calling search.messages');
+    webClient.search.messages({query: msg, sort: 'timestamp'})
+        .then(result => result.messages.matches.filter(item => item.channel.name === channel))
+        .then(findMessagesInChannel => {
+            if (findMessagesInChannel.length > 0) {
+                console.log(findMessagesInChannel);
+
+                let message = channel + '채널에서 *\'' + msg +'\'* 메세지를 ' + findMessagesInChannel.length + '개 찾았습니다'
+                    + findMessagesInChannel.map(item => '\n[' + new Date(Number(item.ts)) +'] ' +item.username + ' : ' + item.text);
+
+                res.send(message);
+                return incomingwebhook.send(message);
+            } else {
+                console.log('No matches found');
+            }
+        })
+        .then(res => console.log('Message sent'))
+        .catch(console.error);
+};
+
+module.exports = {getCurrentTime, findMessageInChannel};
--- router/tutorial.js
+++ router/tutorial.js
@@ -5,4 +5,6 @@ const controller = require('../controller/tutorial');
 router.route('/time').get(controller.getCurrentTime);
     // .post(controller.postUserApps);
 
+router.route('/find').get(controller.findMessageInChannel);
+
 module.exports = router;
